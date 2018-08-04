92fce5683db433d30ec70f492b3c4ffb36e16575
rayt579 <rayt579@yahoo.com>
2018-08-04 16:26:54 -0700

    add connected components

--- premium/meeting_rooms_2.py
+++ premium/meeting_rooms_2.py
@@ -1,53 +1,56 @@
-'''
-https://leetcode.com/explore/interview/card/amazon/79/sorting-and-searching/497/
-'''
-
 # Definition for an interval.
-class Interval(object):
+class Interval:
     def __init__(self, s=0, e=0):
         self.start = s
         self.end = e
-    def __eq__(self, other):
-        print('Start: Compare {} to {}'.format(self.start, other.start))
-        print('End: Compare {} to {}'.format(self.end, other.end))
+    def __lt__(self, other):
+        return self.end < other.end
 
-        return self.start == other.start and self.end == other.end
-
-class Solution(object):
+import heapq
+class Solution:
     def minMeetingRooms(self, intervals):
         """
         :type intervals: List[Interval]
         :rtype: int
         """
-        return self.min_meeting_rooms_bf(intervals)
+        return self.min_meeting_rooms_array(intervals)
 
-    def min_meeting_rooms_bf(self, intervals):
-        if not intervals:
+    def min_meeting_rooms_array(self, intervals):
+        if not intervals or len(intervals) == 0:
             return 0
+        starts = [i.start for i in intervals]
+        ends = [i.end for i in intervals]
+        starts.sort()
+        ends.sort()
+        rooms, endItr = 0, 0
 
-        min_rooms = 1
+        for i in range(len(starts)):
+            if starts[i] < ends[endItr]:
+                rooms += 1
+            else:
+                endItr += 1
+        return rooms
+
+    def min_meeting_rooms_heapq(self, intervals):
+        if not intervals or len(intervals) == 0:
+            return 0
+        rooms = []
+        intervals.sort(key= lambda i: i.start)
+        heapq.heappush(rooms, (intervals[0].end, intervals[0]))
         for i in range(1, len(intervals)):
-            overlap_count = 0
-            for j in range(i):
-                print(i, j)
-                if self.has_overlap(intervals[i], intervals[j]):
-                    overlap_count += 1
-                elif intervals[i] == intervals[j]:
-                    overlap_count += 2
-            if overlap_count == i:
-                min_rooms += 1
-        return min_rooms
+            interval = heapq.heappop(rooms)[1]
+            if intervals[i].start >= interval.end:
+                interval.end = intervals[i].end
+            else:
+                heapq.heappush(rooms, (intervals[i].end, intervals[i]))
+            heapq.heappush(rooms, (interval.end, interval))
+        return len(rooms)
 
-    def has_overlap(self, a, b):
-        return a.start < b.end and b.start < a.end
-
-    def comp(self, i1, i2):
-        return i1.start == i2.start and i1.end == i2.end
+a = [Interval(0, 30), Interval(5,10), Interval(15,20)]
+b = [Interval(7,10), Interval(2,4)]
+c = [Interval(5, 8), Interval(6,8)]
 
 sol = Solution()
-a = [Interval(15, 20), Interval(5, 10), Interval(0, 30)]
-b = [Interval(7, 10), Interval(2,4)]
-c = [Interval(1, 5), Interval(8, 9), Interval(8,9)]
-#print('Expecting 2: {}'.format(sol.minMeetingRooms(a)))
-#print('Expecting 1: {}'.format(sol.minMeetingRooms(b)))
-print('Expecting 2: {}'.format(sol.minMeetingRooms(c)))
+#print('Expect 2: {}'.format(sol.minMeetingRooms(a)))
+#print('Expect 1: {}'.format(sol.minMeetingRooms(b)))
+print('Expect : {}'.format(sol.minMeetingRooms(c)))
